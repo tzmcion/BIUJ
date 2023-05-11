a1d16a34665e42a6729243c1b328a04734afb7cd
tzmcion <tymsonekjelonek@gmail.com>
2023-05-11 07:55:24 -0400

    added database colaboration

--- src/Pages/SubPage/SubPage.tsx
+++ src/Pages/SubPage/SubPage.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {ReactElement, useState, useEffect, useCallback} from 'react'
 import { useParams } from 'react-router'
+import axios from 'axios';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import QuestionCard from '../../Components/QuestionCard/QuestionCard';
@@ -8,32 +9,69 @@ import "./SubPage.scss"
 import useTitle from '../../Components/useTitle';
 import AddQuestion from '../../Components/AddQuestion/AddQuestion';
 
-export default function SubPage() {
+interface Data{
+  answers:string
+  comment:string,
+  correct:string,
+  id:number,
+  nick:string,
+  question:string,
+  type:"closed" | "open"
+}
+
+export default function SubPage():ReactElement {
     const { id } = useParams();
     useTitle(`${id?.toLocaleUpperCase()} / Bioinformatyka UJ`);
 
-    const handleNext = ():void =>{
+    const [data,setData] = useState<Data | null>();
 
+    const handleNext = useCallback(()=>{
+        axios.get(`http://localhost:8000/get_question/${id}`).then(res =>{
+          if("ERROR" in res.data){
+            setData(null);
+          }else{
+            setData(res.data!);
+          }
+        });
+    },[id])
+
+
+    const handleAdd = (question:string,answers:Array<string>,nick:string,comment:string,type:string,correct_answer:string):void =>{
+        const data = {
+          question:question,
+          answers:JSON.stringify(answers),
+          nick:nick,
+          comment:comment,
+          type:type,
+          correct:correct_answer,
+          table:id
+        }
+        axios.post("http://localhost:8000/add",data).then(res=>{
+          console.log("data added!");
+        });
     }
 
-    const handleAdd = ():void =>{
+    useEffect(()=>{
+      handleNext();
+    },[handleNext])
 
-    }
   return (
     <div>
       <Header />
       <div className='SubData'>
         <h2 className='ID'>{id}</h2>
+        {data &&
             <QuestionCard 
-              answers={["a","b","c","d"]}
-              comment='fajnie' 
-              type='closed' 
-              correct_answer='a' 
-              question='a'
-              id={1}
-              user="Anonim"
+              answers={JSON.parse(data.answers)}
+              comment={data.comment}
+              type={data.type}
+              correct_answer={data.correct} 
+              question={data.question}
+              id={data.id}
+              user={data.nick}
               handleNext={handleNext}
               />
+        }
               <AddQuestion handleAdd={handleAdd}/>
       </div>
       <Footer />
